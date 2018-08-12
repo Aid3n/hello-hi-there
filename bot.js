679af6ad7d18b02883467ba8cac51b337068cd28
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-08-12 17:37:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
 
-const client = new Discord.Client();
+var bot = new Discord.Client();
 
-client.on("ready", function() {
+bot.on("ready", function() {
     console.log("Ready");
 });
 
-client.on("message", function(message) {
+bot.on("message", function(message) {
     if (message.author.equals(bot.user)) return;
 
     if (message.content == "hello")
         message.channel.sendMessage("Hi, there!");
     }
-});
+,);
 
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
